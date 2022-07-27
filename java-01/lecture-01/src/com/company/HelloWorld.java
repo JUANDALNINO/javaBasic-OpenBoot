0817b0f80215b16d00b308a10b1f43abfd3e5e3e
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-07-27 16:48:41 -0500

    New notes on day

--- java-01/lecture-01/src/com/company/HelloWorld.java
+++ java-01/lecture-01/src/com/company/HelloWorld.java
@@ -1,7 +1,21 @@
 package com.company;
 
 public class HelloWorld {
+
+    /**
+     * Esto es la documentación de la app
+     * @param args
+     */
+
     public static void main(String[] args) {
         System.out.println("Hello world from helloWorld class");
     }
 }
+
+// Estos son comentarios
+/*
+
+    Esto es otro comentario
+    1. First step
+    2. Second step
+*/
---
+++ java-01/lecture-01/src/com/company/Types.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Types {
+    public static void main(String[] args) {
+        // 1. tipo de dato + id(identificador) = valor
+
+        // 2. tipo + id
+        //    id = valor
+
+        // enteros
+        byte number1 = 1; //1byte
+        short number2 = 2; //2byte
+        int number3 = 3; //4byte
+        long number4 = 4; //8byte
+
+        // Punto flotante
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        // caracter
+        char caracter1 = 'a';
+
+        // Booleanos
+        boolean trueBoo = true;
+        boolean falseBoo = false;
+
+        // Strings textos
+        String nombre = "Juan";
+        String apellido = "Nino";
+
+        // Tipos envoltorios
+        Integer numero = null;
+        Long numero2 = 2L;
+    }
+}
