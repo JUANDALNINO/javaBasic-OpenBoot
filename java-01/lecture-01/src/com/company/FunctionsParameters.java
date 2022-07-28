0b932b0f5444f433b530e99874d92dd90d6003ff
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-07-27 23:06:17 -0500

    New changes

--- java-01/lecture-01/src/com/company/FunctionsParameters.java
+++ java-01/lecture-01/src/com/company/FunctionsParameters.java
@@ -11,10 +11,13 @@ public class FunctionsParameters {
 
         String hello = returnHello();
         System.out.println(hello);
+
+        int suma = sum(10, 10);
+        System.out.println(suma);
     }
 
     // Métodos => es una función
-    public static void helloWorld() {
+    private static void helloWorld() {
         System.out.println("Hello World from a method");
     }
 
@@ -27,4 +30,17 @@ public class FunctionsParameters {
     public static String returnHello() { // String => text - Puede ser con cualquier tipo de dato
         return "Hello. I'm a return";
     }
+
+    private static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+
+    // Modificadores
+    // Public y Private
+
+    // Protected => Modificador por paquete
+
+    // Sobrecarga de funciones => cuando son dos funciones nombradas igual pero con diferentes parametros
+
 }
---
+++ java-01/lecture-01/src/com/company/FunctionsTest.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class FunctionsTest {
+
+    public static void main(String[] args) {
+        FunctionsParameters.byeWorld("Eternidad"); // Metodo publico
+    }
+
+}
