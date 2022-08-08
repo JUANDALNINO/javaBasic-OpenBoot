8903cbd65d264180070c4787a41905f9e6acb91f
Juan David Lizarralde Niño <nino.davidgo@gmail.com>
2022-08-08 16:30:09 -0500

    break and continue in Java

---
+++ java-01/lecture-01/src/com/company/ContinueAndBreak.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class ContinueAndBreak {
+    public static void main(String[] args) {
+
+        int count = 0;
+        while(count<10) {
+            System.out.println("Hello World " + count);
+            count++;
+
+        }
+        System.out.println("End to process");
+    }
+}
+
+
